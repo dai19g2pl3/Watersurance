34290e7f9439240f3c522de5f94936d78fef633d
edunter <eagr1999@gmail.com>
2019-04-09 20:18:55 +0100

    Atualização Back End - Auth

--- backEnd/watersurance/src/main/java/com/dai/watersurance/WatersuranceApplication.java
+++ backEnd/watersurance/src/main/java/com/dai/watersurance/WatersuranceApplication.java
@@ -1,11 +1,26 @@
 package com.dai.watersurance;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {
+		WatersuranceApplication.class,
+		Jsr310JpaConverters.class
+})
 public class WatersuranceApplication {
 
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Lisbon"));
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WatersuranceApplication.class, args);
 	}
