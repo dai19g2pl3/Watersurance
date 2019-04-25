d889c05f542097cc845167fe94ccefad3cd16a62
edunter <eagr1999@gmail.com>
2019-04-25 11:35:20 +0100

    login and register

--- frontOffice/js/controllers/loginController.js
+++ frontOffice/js/controllers/loginController.js
@@ -1,16 +1,21 @@
 $(document).ready(function () {
+    validatorLogin();
+
+    $('#btnSignUpCancel').click(function(){
+        $('#signUpModal').modal('toggle');
+    });
+
     $('#loginForm').on('submit', function (e) {
         e.preventDefault();
-        login();
+        validatorLogin();
     })
 });
 
 function login() {
-    alert("arroz");
     var data = {};
-    data.email = document.getElementById('emailLogin').value
-    data.password = document.getElementById('pwdLogin').value
-    alert(JSON.stringify(data));
+    data.email = document.getElementById('email').value
+    data.password = document.getElementById('pwd').value
+
     fetch('http://localhost:8080/api/auth/signin', {
         headers: { 'Content-Type': 'application/json' },
         method: 'POST',
@@ -20,11 +25,35 @@ function login() {
     }).then(response =>
         response.json().then(json => {
             if (!response.ok) {
-                alert(json);
                 return Promise.reject(json);
             }
+            document.getElementById("loginForm").reset();
             console.log("submitted with success");
             return json;
         })
     )
+}
+
+function validatorLogin() {
+    let validator = new Validator(document.querySelector('form[name="loginForm"]'), function(err, res) {
+        if (res) {
+            login();
+        }
+    }, {
+            rules: {
+                customMinLength: function (value, params) {
+                        return this.min(value.replace(/\s{2,}/g, ' ').length, params);
+                }
+            },
+            messages: {
+                en: {
+                    customMinLength: {
+                            empty: 'Insira a sua password',
+                            incorrect: 'Introdoziu menos de {0} carateres'
+                        }
+                }
+                
+            }
+        });
+
 }
