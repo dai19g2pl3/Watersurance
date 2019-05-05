7a1b450e4cc3527ea25219935fc32272e74cf360
Manuel Diogo <diogo.manuel.diogo.1999@gmail.com>
2019-05-06 00:00:21 +0100

    organizaçao crl

--- backOffice/.history/src/views/Base/Tables/Tables.test_20190416161734.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import TableUser from "./TableUser";
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<TableUser />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
