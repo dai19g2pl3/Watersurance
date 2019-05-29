4ceed2bcd89bdf34b0549241c72b5bdec1c02717
edunter <eagr1999@gmail.com>
2019-05-29 01:29:06 +0100

    frontOffice - login portela

--- frontOffice/js/controllers/loginController.js
+++ frontOffice/js/controllers/loginController.js
@@ -1,58 +1,64 @@
-$(document).ready(function () {
-    validatorLogin();
+$(document).ready(function() {
+  validatorLogin();
 
-    $('#loginForm').on('submit', function (e) {
-        e.preventDefault();
-        validatorLogin();
-    })
+  $("#loginForm").on("submit", function(e) {
+    e.preventDefault();
+    validatorLogin();
+  });
 });
 
 function login() {
-    var data = {};
-    data.email = document.getElementById('loginEmail').value;
-    data.password = document.getElementById('loginPwd').value;
-    
-    fetch('https://watersurance-api.herokuapp.com/api/auth/signin', {
-        headers: { 'Content-Type': 'application/json' },
-        method: 'POST',
-        credentials: 'include',
-        mode: 'cors',
-        body: JSON.stringify(data)
-    }).then(response =>
-        response.json().then(json => {
-            if (!response.ok) {
-                return Promise.reject(json);
-            }
-            //localStorage.setItem('token', json.accessToken);
-            //location.href = 'http://localhost:3000/#/dashboard';
-            document.getElementById("loginForm").reset();
-            return json;
-        })
-    ).catch((err) => {
-        console.log(err);
+  var data = {};
+  data.email = document.getElementById("loginEmail").value;
+  data.password = document.getElementById("loginPwd").value;
+
+  fetch("https://watersurance-api.herokuapp.com/api/auth/signin", {
+    headers: { "Content-Type": "application/json" },
+    method: "POST",
+    credentials: "include",
+    mode: "cors",
+    body: JSON.stringify(data)
+  })
+    .then(response =>
+      response.json().then(json => {
+        if (!response.ok) {
+          return Promise.reject(json);
+        }
+
+        document.cookie = "accessToken=" + json.accessToken + "; path=/;";
+        location.href = 'http://localhost:3000/#/dashboard';
+        document.getElementById("loginForm").reset();
+        return json;
+      })
+    )
+    .catch(err => {
+      console.log(err);
     });
 }
 
 function validatorLogin() {
-    let validator = new Validator(document.querySelector('form[name="loginForm"]'), function(err, res) {
-        if (res) {
-            login();
+  let validator = new Validator(
+    document.querySelector('form[name="loginForm"]'),
+    function(err, res) {
+      if (res) {
+        login();
+      }
+    },
+    {
+      rules: {
+        customMinLength: function(value, params) {
+          return this.min(value.replace(/\s{2,}/g, " ").length, params);
         }
-    }, {
-            rules: {
-                customMinLength: function (value, params) {
-                        return this.min(value.replace(/\s{2,}/g, ' ').length, params);
-                }
-            },
-            messages: {
-                en: {
-                    customMinLength: {
-                            empty: 'Insira a sua password',
-                            incorrect: 'Introdoziu menos de {0} carateres'
-                        }
-                }
-                
-            }
-        });
+      },
+      messages: {
+        en: {
+          customMinLength: {
+            empty: "Insira a sua password",
+            incorrect: "Introdoziu menos de {0} carateres"
+          }
+        }
+      }
+    }
+  );
 
 }
--- frontOffice/js/controllers/logoutController.js
+++ frontOffice/js/controllers/logoutController.js
@@ -1,11 +1,80 @@
 $(document).ready(function () {
 
+    setInterval(() => {
+        if(!(doesCookieExists('accessToken') == null)) {
+            replaceWithLogout();
+        }
+    }, 1000 );
+
+    if(!(doesCookieExists('accessToken') == null)) {
+        replaceWithLogout();
+    }
+
+    $('#logoutNav').on('click', function (e) {
+        e.preventDefault();
+        logout();
+    })
+});
+
+function doesCookieExists(cookieName) {
+    var dc = document.cookie;
+    var prefix = cookieName + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+        end = dc.length;
+        }
+    }
+
+    return decodeURI(dc.substring(begin + prefix.length, end));
+} 
+
+function logout() {
+    document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
+    replaceWithLogin();
+}
+
+function replaceWithLogout() {
+    const loginNav = document.getElementById('loginNav');
+    loginNav.remove();
+
+    const logLi = document.getElementById('logLi');
+    const a = document.createElement("a");
+    a.href = '';
+    a.id = 'logoutNav';
+    a.text = 'Logout'
+    logLi.appendChild(a);       
+}
+
+function replaceWithLogin() {
+    const loginNav = document.getElementById('logoutNav');
+    loginNav.remove();
+
+    const logLi = document.getElementById('logLi');
+    const a = document.createElement("a");
+    a.href = '';
+    a.dataset.toggle = 'modal';
+    a.dataset.target = '#loginModal';
+    a.id = 'loginNav';
+    a.text = 'Login'
+    logLi.appendChild(a);   
+}
+/* Server cookie
+$(document).ready(function () {
+
     const value = doesHttpOnlyCookieExist('token');
     
     if(doesHttpOnlyCookieExist('token')) {
         replaceWithLogout();
     } 
-
+    
     $('#logoutNav').on('click', function (e) {
         e.preventDefault();
         logout();
@@ -52,7 +121,8 @@ function replaceWithLogout() {
         logLi.appendChild(a);       
 }
 
-/*$(document).ready(function () {
+/* Local Storage
+$(document).ready(function () {
     if(localStorage.getItem('token') !== null) {
         replaceWithLogout();
     }
